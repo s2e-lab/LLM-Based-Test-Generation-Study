9b9a3ae79369da86bf3bcc8691fda5ef5fc88b58
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-02-11 21:41:37 -0500

    git updated

--- RQ1_Prompt_Elements/test_file_generation.py
+++ RQ1_Prompt_Elements/test_file_generation.py
@@ -6,7 +6,7 @@ responses = json.load(open('filter_openai_Part3.json'))
 
 for response in responses:
     print(response['choices'][0]["text"])
-    with open('testPart3/id_'+str(response['id'])+'Test.java', 'w') as f:
+    with open('/Users/lsiddiqsunny/Documents/Notre Dame/Research/Deep-Context-Aware-CodeGeneration/RQ1_Prompt_Elements/ResearchData/src/test/Java/testPart3/id_'+str(response['id'])+'Test.java', 'w') as f:
         f.write(response['choices'][0]["text"])
         f.close()
         
