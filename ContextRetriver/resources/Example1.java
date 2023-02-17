97255f17e2c687b3bcae7e419013ca61996c7e8e
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-02-17 01:11:52 -0500

    updated code

---
+++ ContextRetriver/resources/Example1.java
@@ -0,0 +1,52 @@
+
+// javac Example1.java --release 8 && jar cvf Example1.jar *.class && rm *.class
+public class Example1 {
+    public static void main(String[] args) {
+        Shape s;
+        // is even?
+        if (Integer.parseInt(args[0]) % 2 == 0) {
+            s = new Circle();
+        } else {
+            s = new Rectangle();
+        }
+        s.draw();
+    }
+}
+// SSA version:
+//public class Example1 {
+//    public static void main(String[] v1) {
+//        int v3 = 0;
+//        String v4 = v1[v3];
+//        int v6 = Integer.parseInt(v4);
+//        int v7 = 2;
+//        int v8 = v6 % v7;
+//
+//        if ( v8 == v3 ) {
+//            v11 = new Circle();
+//        } else {
+//            v9 = new Rectangle();
+//        }
+//        Shape v13 = Φ(v11, v9);
+//        v13.draw();
+//    }
+//}
+
+interface Shape {
+    void draw();
+}
+class Circle implements Shape{
+    public void draw(){
+        System.out.println("Draw Circle");
+    }
+}
+class Rectangle implements Shape{
+    public void draw(){
+        System.out.println("Draw Rectangle");
+    }
+}
+
+class Triangle implements Shape{
+    public void draw(){
+        System.out.println("Draw Triangle");
+    }
+}
--- ContextRetriver/src/main/java/WalaSrcAnalysis.java
+++ ContextRetriver/src/main/java/WalaSrcAnalysis.java
@@ -1,2 +1,190 @@
-package PACKAGE_NAME;public class WalaSrcAnalysis {
+import com.ibm.wala.cast.ir.ssa.AstIRFactory;
+import com.ibm.wala.cast.java.client.ECJJavaSourceAnalysisEngine;
+import com.ibm.wala.cast.java.ipa.callgraph.JavaSourceAnalysisScope;
+import com.ibm.wala.cast.java.translator.jdt.ecj.ECJClassLoaderFactory;
+import com.ibm.wala.classLoader.ClassLoaderFactory;
+import com.ibm.wala.classLoader.JarFileModule;
+import com.ibm.wala.classLoader.SourceFileModule;
+import com.ibm.wala.ipa.callgraph.*;
+import com.ibm.wala.ipa.callgraph.impl.Util;
+import com.ibm.wala.ipa.callgraph.propagation.InstanceKey;
+import com.ibm.wala.ipa.callgraph.propagation.SSAPropagationCallGraphBuilder;
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.ssa.SSAOptions;
+import com.ibm.wala.ssa.SymbolTable;
+import com.ibm.wala.util.CancelException;
+import com.ibm.wala.util.config.FileOfClasses;
+import com.ibm.wala.viz.viewer.WalaViewer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+public class WalaSrcAnalysis {
+
+    public static String EXCLUSION_FILE = "./resources/Java60RegressionExclusions.txt";
+    public static String JAVA_RUNTIME_17 = "./resources/jdk-17.0.1/rt.jar";
+    public static String JAVA_RUNTIME_8 = "./resources/jre1.8.0_121_debug/lib/rt.jar";
+
+
+    public static JavaSourceAnalysisScope createScope(File sourceFile) throws IOException {
+        JavaSourceAnalysisScope scope = new JavaSourceAnalysisScope();
+        // adds primordial (built-in) classes
+        scope.addToScope(scope.getPrimordialLoader(), new JarFileModule(new JarFile(JAVA_RUNTIME_17, false)));
+        // adds application classes (source code)
+        scope.addToScope(scope.getSourceLoader(), new SourceFileModule(sourceFile, sourceFile.getName(), null));
+        // sets exclusion file
+        scope.setExclusions(new FileOfClasses(new FileInputStream(EXCLUSION_FILE)));
+        return scope;
+    }
+
+
+    public static CallGraph buildNCfaCallGraph(JavaSourceAnalysisScope scope, IClassHierarchy classHierarchy, int n) throws CallGraphBuilderCancelException {
+        AnalysisOptions options = new AnalysisOptions();
+        options.setAnalysisScope(scope);
+
+        SSAOptions ssaOptions = new SSAOptions();
+        ssaOptions.setDefaultValues(SymbolTable::getDefaultValue);
+
+        options.setSSAOptions(ssaOptions);
+        Iterable<Entrypoint> entrypoints = Util.makeMainEntrypoints(scope.getSourceLoader(), classHierarchy);
+        options.setEntrypoints(entrypoints);
+        AnalysisCache analysisCache = new AnalysisCacheImpl(AstIRFactory.makeDefaultFactory(), options.getSSAOptions());
+        SSAPropagationCallGraphBuilder builder = Util.makeNCFABuilder(n, options, analysisCache, classHierarchy, scope);
+
+        return builder.makeCallGraph(options, null);
+    }
+
+
+    public static void main(String[] args) throws IOException, CancelException, ClassHierarchyException {
+
+//      File project = new File("/Users/lsiddiqsunny/Documents/Notre Dame/Research/Deep-Context-Aware-CodeGeneration/EvoSuiteBenchmark/1_tullibee/src/main/java/com/ib/client/AnyWrapperMsgGenerator.java");
+        File project = new File("./resources/Example1.java");
+        System.out.println(new File(".").getAbsolutePath());
+        ECJJavaSourceAnalysisEngine engine = new ECJJavaSourceAnalysisEngine();
+        engine.addSystemModule(new JarFileModule(new JarFile(JAVA_RUNTIME_17, false)));
+        engine.setExclusionsFile(EXCLUSION_FILE);
+        engine.addSourceModule(new SourceFileModule(project, project.getName(), null));
+        engine.buildAnalysisScope();
+        IClassHierarchy cha = engine.buildClassHierarchy();
+        System.out.println("Classes: " + cha.getNumberOfClasses());
+        System.out.println(cha.getScope().toString());
+//        CallGraph callGraph = engine.buildDefaultCallGraph();
+        CallGraphBuilder<InstanceKey> builder = engine.defaultCallGraphBuilder();
+//        CallGraphBuilder<InstanceKey> builder = engine.defaultCallGraphBuilder();
+//        AnalysisOptions options = engine.getOptions();
+
+        CallGraph callGraph = builder.makeCallGraph(engine.getOptions(), null);
+
+        new WalaViewer(callGraph, builder.getPointerAnalysis());
+
+        // creates class hierarchy
+//        JavaSourceAnalysisScope scope = createScope(project);
+//        ClassLoaderFactory factory = new ECJClassLoaderFactory(scope.getExclusions());
+//        IClassHierarchy classHierarchy = ClassHierarchyFactory.make(scope, factory);
+//
+//        System.out.println("Classes: " + classHierarchy.getNumberOfClasses());
+//        System.out.println(classHierarchy.getScope().toString());
+
+
+    }
 }
+
+
+///**
+// * This is a simple example WALA application that uses WALA to build a call graph from a source file.
+// *
+// * @author Joanna C. S. Santos
+// */
+//public class WalaExampleSrcAnalysis {
+//    private static String EXCLUSION_FILE = "./src/main/resources/Java60RegressionExclusions.txt";
+//    private static String JAVA_RUNTIME = "./src/main/resources/jdk-17.0.1/rt.jar";
+//
+//    public static JavaSourceAnalysisScope createScope(File sourceFile) throws IOException {
+//        JavaSourceAnalysisScope scope = new JavaSourceAnalysisScope();
+//        // adds primordial (built-in) classes
+//        scope.addToScope(scope.getPrimordialLoader(), new JarFile(JAVA_RUNTIME, false));
+//        // adds application classes (source code)
+//        scope.addToScope(scope.getSourceLoader(), new SourceFileModule(sourceFile, sourceFile.getName(), null));
+//        // sets exclusion file
+//        scope.setExclusions(new FileOfClasses(new FileInputStream(EXCLUSION_FILE)));
+//        return scope;
+//    }
+//
+//    public static CallGraph buildChaCallGraph(JavaSourceAnalysisScope scope, IClassHierarchy classHierarchy) throws CancelException {
+//        CHACallGraph cg = new CHACallGraph(classHierarchy, true);
+//        Iterable<Entrypoint> entrypoints = Util.makeMainEntrypoints(scope.getSourceLoader(), classHierarchy);
+//        cg.init(entrypoints);
+//        return cg;
+//    }
+//
+//    public static CallGraph buildRtaCallGraph(AnalysisScope scope, IClassHierarchy classHierarchy) throws CallGraphBuilderCancelException {
+//        AnalysisOptions options = new AnalysisOptions();
+//        Iterable<Entrypoint> entrypoints = Util.makeMainEntrypoints(scope, classHierarchy);
+//        options.setEntrypoints(entrypoints);
+//        AnalysisCache analysisCache = new AnalysisCacheImpl();
+//        CallGraphBuilder<InstanceKey> cgBuilder = Util.makeRTABuilder(options, analysisCache, classHierarchy, scope);
+//        return cgBuilder.makeCallGraph(options, null);
+//    }
+//
+//    public static CallGraph buildNCfaCallGraph(AnalysisScope scope, IClassHierarchy classHierarchy, int n) throws CallGraphBuilderCancelException {
+//        AnalysisOptions options = new AnalysisOptions();
+//        Iterable<Entrypoint> entrypoints = Util.makeMainEntrypoints(scope, classHierarchy);
+//        options.setEntrypoints(entrypoints);
+//        AnalysisCache analysisCache = new AnalysisCacheImpl();
+//        SSAPropagationCallGraphBuilder builder = Util.makeNCFABuilder(n, options, analysisCache, classHierarchy, scope);
+//
+//        return builder.makeCallGraph(options, null);
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        String exampleFile = "./src/main/resources/Example1.java";
+//
+//        // creates analysis scope
+//        JavaSourceAnalysisScope scope = createScope(new File(exampleFile));
+//
+//        // creates class hierarchy
+//        IClassHierarchy classHierarchy = ClassHierarchyFactory.make(scope);
+//
+//
+//        // CHA Call Graph
+//        CallGraph chaCallGraph = buildChaCallGraph(scope, classHierarchy);
+//
+//        // Converts to DOT format
+//        DotUtil.dotify(chaCallGraph,
+//                cgNode -> cgNode.getMethod().getSignature()
+//                , "cha-cg-" + exampleFile.split("/")[4] + ".dot",
+//                null,
+//                null);
+//
+//
+//        for (CGNode node : chaCallGraph.getEntrypointNodes()) {
+//            System.out.println(node.getIR());
+//        }
+//
+//        // RTA
+//        CallGraph rtaCallGraph = buildRtaCallGraph(scope, classHierarchy);
+//        Graph<CGNode> prunedRta = GraphSlicer.prune(rtaCallGraph, n -> n.getMethod().getDeclaringClass().getClassLoader().getReference().equals(ClassLoaderReference.Application));
+//        DotUtil.dotify(prunedRta,
+//                cgNode -> cgNode.getMethod().getSignature(),
+//                "rta-cg-" + exampleFile.split("/")[4] + ".dot",
+//                null,
+//                null);
+//
+//
+//        // n-CFA
+//        CallGraph oneCFA = buildNCfaCallGraph(scope, classHierarchy, 1);
+//        Graph<CGNode> prunedCFA = GraphSlicer.prune(oneCFA, n -> n.getMethod().getDeclaringClass().getClassLoader().getReference().equals(ClassLoaderReference.Application));
+//        DotUtil.dotify(prunedCFA,
+//                cgNode -> cgNode.getMethod().getSignature() + "@" + cgNode.getContext(),
+//                "1-cfa-cg-" + exampleFile.split("/")[4] + ".dot",
+//                null,
+//                null);
+//
+//    }
+//
+//
+//}
