0e8740e8d431b22e5be2ff4f791e77f0cbe3137b
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2023-02-24 13:19:06 -0500

    Removed deprecated dependency (that project was abondoned), replaced by a newwer fork from clifton labs; Added a dependency to apache commons text to help create named template strings

---
+++ ContextRetriver/src/main/resources/RQ1UnitTestTemplate.java
@@ -0,0 +1,10 @@
+// %sTest.java
+package %s;
+%s
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+* Unit tests of {@link %s}.
+* It contains %s test cases for the {@link %s#%s()} method.
+*/
+class %sTest {
