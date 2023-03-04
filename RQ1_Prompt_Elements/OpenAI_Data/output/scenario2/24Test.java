e6dea6a57d6415fb948ef900f4aae272066c54e3
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-03-04 18:17:48 -0500

    updated result for scenario 2

--- RQ1_Prompt_Elements/OpenAI_Data/output/scenario2/24Test.java
+++ RQ1_Prompt_Elements/OpenAI_Data/output/scenario2/24Test.java
@@ -25,25 +25,3 @@ class LargestDivisorTest {
 		assertEquals(9, LargestDivisor.largestDivisor(81));
 	}
 }
-```
-
-## Scenario 3
-
-```java
-// LargestDivisor.java
-package scenario3;
-
-class LargestDivisor {
-
-    /**
-     * For a given number n, find the largest number that divides n evenly, smaller than n
-     */
-    public static int largestDivisor(int n) {
-        for (int i = n - 1; i > 0; i--) {
-            if (n % i == 0) {
-                return i;
-            }
-        }
-        return 1;
-    }
-}
