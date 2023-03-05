13959a42bdc4e32d3002a45c9bc4958cf7432e4d
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-03-05 18:12:15 -0500

    updated with result for RQ1 Sc1 and Sc2

---
+++ RQ1_Prompt_Elements/RQ1_Result/coverage_result/src/test/java/scenario1/FindClosestElementsTest.java
@@ -0,0 +1,165 @@
+// FindClosestElementsTest.java
+package scenario1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class of {@link FindClosestElements}.
+ * It contains ten unit test cases for the {@link FindClosestElements#findClosestElements(List)} method.
+ */
+class FindClosestElementsTest {
+
+    @Test
+    void testFindClosestElements_1() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_2() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_3() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_4() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_5() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_6() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_7() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_8() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_9() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindClosestElements_10() {
+        List<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+        numbers.add(2.0);
+        numbers.add(3.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        List<Double> expected = new ArrayList<Double>();
+        expected.add(1.0);
+        expected.add(2.0);
+        List<Double> actual = FindClosestElements.findClosestElements(numbers);
+        assertEquals(expected, actual);
+    }
+}
---
+++ RQ1_Prompt_Elements/RQ1_Result/coverage_result/src/test/java/scenario2/HistogramTest.java
@@ -0,0 +1,96 @@
+// HistogramTest.java
+package scenario2;
+
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class of {@link Histogram}.
+ * It contains ten unit test cases for the {@link Histogram#histogram(String)} method.
+ */
+class HistogramTest {
+
+    @Test
+    void testHistogram() {
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+            }
+        }, Histogram.histogram("a b a b"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c a b"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c a b c c"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c a b c c a b"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c a b c c a b c c"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c a b c c a b c c a b"));
+        assertEquals(new HashMap<String, Integer>() {
+
+            {
+                put("a", 2);
+                put("b", 2);
+                put("c", 2);
+            }
+        }, Histogram.histogram("a b a b c c a b c c a b c c a b c c a b c c"));
+    }
+}
