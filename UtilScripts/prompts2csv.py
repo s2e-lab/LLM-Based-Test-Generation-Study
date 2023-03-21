ad1f90947713f447175a2a5840930d5e25f61ccf
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-03-21 02:06:13 -0400

    updated job script

--- UtilScripts/prompts2csv.py
+++ UtilScripts/prompts2csv.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import re
+import os
 
 
 
@@ -30,13 +31,35 @@ def main(scenario_path):
             print(p)
             csv_file.writerow([p["id"],p["method_signature"], p["classname"]])
 
+def createJob(scenario_path):
+    with open("../scripts/generate_tests_SF110.sh", "w") as f:
+        f.write("#!/bin/bash"+"\n")
+
+        f.write("#$ -M msiddiq3@nd.edu   # Email address for job notification"+"\n")
+        f.write("#$ -m abe            # Send mail when job begins, ends and aborts"+"\n")
+        f.write("#$ -pe smp 1     # Specify parallel environment and legal core size"+"\n")
+        f.write("#$ -q long           # Specify queue"+"\n")
+        f.write("#$ -N  RQ1_SF110_2K_4K"+"\n")
+    
+    for token in [2000,4000]:
+        for project in os.listdir(scenario_path):
+            if project.endswith(".csv"):
+                continue
+            with open("../scripts/generate_tests_SF110.sh", "a") as f:
+                f.write(f"python generate_tests_openai.py -t {token} -d SF110 -q RQ1 -p \"RQ1_Test_Generation/OpenAI_Data/SF110_input/{project}\"\n")
+
 
 if __name__ == "__main__":
 
     # RQ1 folder
     rq1_folder = "../RQ1_Test_Generation/OpenAI_Data/%s_input/%s_prompt.json"
     main(rq1_folder % ("HumanEvalJava", "original"))
-    for p in ["100_jgaap", "10_water-simulator", "11_imsmart", "12_dsachat", "13_jdbacl", "14_omjstate", "15_beanbin", "17_inspirento", "19_jmca", "22_byuic", "23_jwbf", "24_saxpath", "25_jni-inchi", "27_gangup", "29_apbsmem", "2_a4j", "30_bpmail", "31_xisemele", "32_httpanalyzer", "33_javaviewcontrol", "34_sbmlreader2", "36_schemaspy", "39_diffi", "40_glengineer", "41_follow", "42_asphodel", "43_lilith", "47_dvd-homevideo", "49_diebierse", "4_rif", "51_jiprof", "52_lagoon", "54_db-everywhere", "55_lavalamp", "56_jhandballmoves", "57_hft-bomberman", "59_mygrid", "5_templateit", "60_sugar", "63_objectexplorer", "64_jtailgui", "65_gsftp", "67_gae-app-manager", "68_biblestudy", "69_lhamacaw", "6_jnfe", "71_ext4j", "73_fim1", "74_fixsuite", "75_openhre", "76_dash-framework", "7_sfmis", "81_javathena", "82_ipcalculator", "84_ifx-framework", "86_at-robots2-j", "88_jopenchart", "8_gfarcegestionfa", "91_classviewer", "93_quickserver", "94_jclo"]:
+
+
+    projectList =  ["5_templateit","71_ext4j","103_sweethome3d","14_omjstate","69_lhamacaw","82_ipcalculator","12_dsachat","86_at-robots2-j","2_a4j","17_inspirento","7_sfmis","32_httpanalyzer","29_apbsmem","10_water-simulator","56_jhandballmoves","81_javathena","39_diffi","57_hft-bomberman","91_classviewer","43_lilith","34_sbmlreader2","33_javaviewcontrol","47_dvd-homevideo","67_gae-app-manager","41_follow","23_jwbf","79_twfbplayer","73_fim1","94_jclo","31_xisemele","64_jtailgui","93_quickserver","68_biblestudy","100_jgaap","4_rif","60_sugar","52_lagoon","6_jnfe","24_saxpath","96_heal","59_mygrid","40_glengineer","42_asphodel","11_imsmart","74_fixsuite","75_openhre","8_gfarcegestionfa","63_objectexplorer","22_byuic","25_jni-inchi","84_ifx-framework","19_jmca","49_diebierse"]
+    # createJob("../RQ1_Test_Generation/OpenAI_Data/%s_input"%("SF110"))
+
+    for p in projectList:
         main(rq1_folder % ("SF110", p))
 
     # RQ2 folder
