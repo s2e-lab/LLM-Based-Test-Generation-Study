f1e7b7ebc3f74db4630511c1568726e937a3b0f1
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2023-03-27 00:00:06 -0400

    Logic for codegen

--- UtilScripts/rename_classname.py
+++ UtilScripts/rename_classname.py
@@ -3,7 +3,9 @@ import re
 
 # Define the regular expression pattern to match the class name
 class_name_pattern = re.compile(r"\nclass\s+(\w+)")
-directory = "../../ICSE23-results/OpenAI/HumanEvalJava-Results/src/test/java/scenario3"
+
+
+directory = "../../ICSE23-results/GPT3.5/HumanEvalJava-Results/src/test/java/original"#"../../ICSE23-results/OpenAI/HumanEvalJava-Results/src/test/java/scenario3"
 to_rename = []
 # Iterate over all the files in the current directory
 for filename in os.listdir(directory):
