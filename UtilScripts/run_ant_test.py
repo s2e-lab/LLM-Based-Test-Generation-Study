1ba183f83a6136eb4a4fd59b91455bbddfb3cb0f
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-03-29 21:00:29 -0400

    updated result

--- UtilScripts/run_ant_test.py
+++ UtilScripts/run_ant_test.py
@@ -2,7 +2,11 @@ import subprocess
 import os
 import time
 
+
 projects = []
+
+scenario = "scenario4"
+token = "4000"
 
 for project in os.listdir("../RQ2_Prompt_Elements/OpenAI_Data/%s_input" % ("SF110")):
     if project.endswith(".csv"):
@@ -11,10 +15,13 @@ for project in os.listdir("../RQ2_Prompt_Elements/OpenAI_Data/%s_input" % ("SF11
         projects.append("_".join(project.split("_")[1:3]))
 
 ignore_projects = ["23_", "40_", "57_", "79_", "84_", "86_"]
+
 source_folder = "/Users/lsiddiqsunny/Documents/Notre_Dame/Research/ICSE23-results/OpenAI/SF110-Results"
 subfolders = [f.path for f in os.scandir(source_folder) if f.is_dir()]
+
 for subfolder in subfolders:
     projectName = subfolder.split("/")[-1]
+
     if projectName not in projects:
         continue
     is_continue = False
@@ -24,7 +31,8 @@ for subfolder in subfolders:
             break
     if is_continue:
         continue
-    print(subfolder)
+
+    print("Running ant test for ", projectName)
     os.chdir(subfolder)
 
     run_ant = True
@@ -35,55 +43,66 @@ for subfolder in subfolders:
         run_count += 1
         if run_count > 5:
             break
-        # Run the "mvn test" command and capture the output
-        process = subprocess.Popen(["ant"], stdout=subprocess.PIPE)
-        output, error = process.communicate(timeout=60)
+        try:
+            # Run the "mvn test" command and capture the output
+            process = subprocess.Popen(["ant"], stdout=subprocess.PIPE)
+            output, error = process.communicate(timeout=600)
 
-        # Print the output of the command
-        test_results = output.decode("utf-8")
+            # Print the output of the command
+            test_results = output.decode("utf-8")
 
-        # Check if the command ran successfully
-        if process.returncode == 0:
-            print("ant test succeeded")
+            # Check if the command ran successfully
+            if process.returncode == 0:
+                print("ant test succeeded")
+                run_ant = False
+            else:
+                print("ant test failed")
+                # parse the compilation output
+                # test_results = test_results.replace("\n  ","")
+
+                lines = test_results.split("\n")
+                print("Output # lines ", len(lines))
+
+                start, end = -1, -1
+                for i in range(0, len(lines)):
+                    line = lines[i]
+                    if start == -1 and "error:" in line:
+                        start = i 
+                    if start >= 0 and (i > start + 1) and "[javac]" in line:
+                        end = i + 1
+
+                run_ant = True if start > 0 else False
+                # collects files that failed compilation
+                to_delete = []
+                output = []
+                for i in range(start, end):
+                    line = lines[i]
+                    if "error:" in line:
+                        parts = line.split()
+                        file = parts[1].split(":")[0]
+                        if "_Test.java" not in file:
+                            continue
+                        location = parts[1].split(":")[1][1:-1]
+                        error = " ".join(parts[2:])
+                        if "error: cannot find symbol" in error:
+                            error_symbol = lines[i + 3].split("[javac]")[1].strip()
+                            error_location = lines[i + 4].split("[javac]")[1].strip()
+                            error = error + " " + error_symbol + " " + error_location
+
+                        to_delete.append(file)
+                        output.append("\t".join([file, location, error]))
+
+                # save the compilation status
+                with open(
+                    f"output_{run_count}_{projectName}_{scenario}_{token}.csv", "w"
+                ) as f:
+                    f.write("\n".join(output))
+
+                # delete the problematic test files
+                for f in to_delete:
+                    if os.path.exists(f):
+                        print("Removing ", f)
+                        os.remove(f)
+        except subprocess.TimeoutExpired:
+            print("ant test timed out")
             run_ant = False
-        else:
-            print("ant test failed")
-            # parse the compilation output
-            # test_results = test_results.replace("\n  ","")
-
-            lines = test_results.split("\n")
-            print("Output # lines ", len(lines))
-            start, end = -1, -1
-            for i in range(0, len(lines)):
-                line = lines[i]
-                if start == -1 and "error:" in line:
-                    start = i + 1
-                if start >= 0 and (i > start + 1) and ("-" * 60) in line:
-                    end = i + 1
-                    break
-
-            run_ant = True if start > 0 else False
-
-            # collects files that failed compilation
-            to_delete = []
-            output = []
-            for line in lines[start:end]:
-                if "error:" in line:
-                    parts = line.split()
-                    file = parts[1].split(":")[0]
-                    if "_Test.java" not in file:
-                        continue
-                    location = parts[1].split(":")[1][1:-1]
-                    error = " ".join(parts[2:])
-                    to_delete.append(file)
-                    output.append("\t".join([file, location, error]))
-
-            # save the compilation status
-            with open(f"output{time.time()}.csv", "w") as f:
-                f.write("\n".join(output))
-
-            # delete the problematic test files
-            for f in to_delete:
-                if os.path.exists(f):
-                    print("Removing ", f)
-                    os.remove(f)
