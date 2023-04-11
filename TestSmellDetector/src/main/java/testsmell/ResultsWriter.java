f72bb9edbbb718c3f8f374562a7e1565f0a311e1
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2023-04-11 17:10:05 -0400

    Minor changes on TsDetect such that I could bulk compute the test smells

--- TestSmellDetector/src/main/java/testsmell/ResultsWriter.java
+++ TestSmellDetector/src/main/java/testsmell/ResultsWriter.java
@@ -16,25 +16,40 @@ public class ResultsWriter {
 
     /**
      * Creates the file into which output it to be written into. Results from each file will be stored in a new file
+     *
      * @throws IOException
      */
-    private ResultsWriter() throws IOException {
-        String time =  String.valueOf(Calendar.getInstance().getTimeInMillis());
-        outputFile = MessageFormat.format("{0}_{1}_{2}.{3}", "Output","TestSmellDetection",time, "csv");
-        writer = new FileWriter(outputFile,false);
+    private ResultsWriter(String suffix) throws IOException {
+        String filenameSuffix = suffix;
+        if (suffix == null || suffix.isEmpty())
+            filenameSuffix = String.valueOf(Calendar.getInstance().getTimeInMillis());
+        outputFile = MessageFormat.format("{0}_{1}_{2}.{3}", "Output", "TestSmellDetection", filenameSuffix, "csv");
+        writer = new FileWriter(outputFile, false);
     }
 
     /**
      * Factory method that provides a new instance of the ResultsWriter
+     * @param suffix the suffix to be added to the output file name (if null, it uses a time stamp of the current time)
+     * @return new ResultsWriter instance
+     * @throws IOException
+     */
+    public static ResultsWriter createResultsWriter(String suffix) throws IOException {
+        return new ResultsWriter(suffix);
+    }
+
+    /**
+     * Factory method that provides a new instance of the ResultsWriter
+     *
      * @return new ResultsWriter instance
      * @throws IOException
      */
     public static ResultsWriter createResultsWriter() throws IOException {
-        return new ResultsWriter();
+        return new ResultsWriter(null);
     }
 
     /**
      * Writes column names into the CSV file
+     *
      * @param columnNames the column names
      * @throws IOException
      */
@@ -44,6 +59,7 @@ public class ResultsWriter {
 
     /**
      * Writes column values into the CSV file
+     *
      * @param columnValues the column values
      * @throws IOException
      */
@@ -53,16 +69,17 @@ public class ResultsWriter {
 
     /**
      * Appends the input values into the CSV file
+     *
      * @param dataValues the data that needs to be written into the file
      * @throws IOException
      */
-    private void writeOutput(List<String> dataValues)throws IOException {
-        writer = new FileWriter(outputFile,true);
+    private void writeOutput(List<String> dataValues) throws IOException {
+        writer = new FileWriter(outputFile, true);
 
-        for (int i=0; i<dataValues.size(); i++) {
+        for (int i = 0; i < dataValues.size(); i++) {
             writer.append(String.valueOf(dataValues.get(i)));
 
-            if(i!=dataValues.size()-1)
+            if (i != dataValues.size() - 1)
                 writer.append(",");
             else
                 writer.append(System.lineSeparator());
